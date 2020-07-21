87e69a383e5f47ab78aabf7f9c11e200ed3308c3
aldeaman <evan@evangould.com>
2020-07-20 20:51:12 -0400

    modified:   __init__.py 	modified:   locale/en-us/test.trigger.skill.dialog 	modified:   locale/en-us/test.trigger.skill.intent

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
-from mycroft import MycroftSkill, intent_file_handler
+rom mycroft import MycroftSkill, intent_handler
 
 
 class SkillTriggerTest(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('test.trigger.skill.intent')
+    @intent_handler('test.trigger.skill.intent')
     def handle_test_trigger_skill(self, message):
         self.speak_dialog('test.trigger.skill')
 
